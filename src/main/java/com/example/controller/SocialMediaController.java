5ed5abc94550cde200e2a2a682acfe65b8f4eac4
Chinedu Ezuma <chineduezuma@yahoo.co.uk>
2024-07-10 22:09:17 +0000

    Prem 13

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -4,10 +4,13 @@ import com.example.entity.Account;
 import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -32,7 +35,7 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/register", method = RequestMethod.POST)
     @PostMapping("/register")
-    public @ResponseBody Account register(@RequestBody Account newUser) {
+    public ResponseEntity<Account> register(@RequestBody Account newUser) {
         // Logic to register a new user
     }
 
@@ -40,7 +43,7 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/login", method = RequestMethod.POST)
     @PostMapping("/login")
-    public @ResponseBody Account addlogin(@RequestBody Account loginRequest) {
+    public ResponseEntity<Account> addlogin(@RequestBody Account loginRequest) {
         // Logic to authenticate user login
     }
 
@@ -48,7 +51,7 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/messages", method = RequestMethod.POST)
     @PostMapping("/messages")
-    public @ResponseBody Message addMessage(@RequestBody Message newMessage) {
+    public ResponseEntity<Message> addMessage(@RequestBody Message newMessage) {
         // Logic to authenticate user login
     }
 
@@ -56,7 +59,7 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/messages", method = RequestMethod.GET)
     @GetMapping("/messages")
-    public @ResponseBody List<Message> findAllMessages() {
+    public ResponseEntity<List<Message>> findAllMessages() {
         // Logic to authenticate user login
     }
 
@@ -64,7 +67,7 @@ public class SocialMediaController {
     
     // @RequestMapping(value = "/messages/{message_id}", method = RequestMethod.GET)
     @GetMapping("/messages/{message_id}")
-    public @ResponseBody Message findMessageByMessageId(@PathVariable Integer messageId) {
+    public ResponseEntity<Message> findMessageByMessageId(@PathVariable Integer messageId) {
         // Logic to retrieve user profile
     }
 
@@ -72,7 +75,7 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/messages/{message_id}", method = RequestMethod.DELETE)
     @DeleteMapping("/messages/{message_id}")
-    public @ResponseBody Integer removeMessageByMessageId(@PathVariable Integer messageId) {
+    public ResponseEntity<Integer> removeMessageByMessageId(@PathVariable Integer messageId) {
         // Logic to retrieve user profile
     }
 
@@ -80,62 +83,30 @@ public class SocialMediaController {
 
     // @RequestMapping(value = "/messages/{message_id}", method = RequestMethod.PATCH)
     @PatchMapping("/messages/{message_id}")
-    public @ResponseBody Integer updatehMessageByMessageId(@PathVariable Integer messageId, @RequestBody Message newMessage) {
+    public ResponseEntity<Integer> updatehMessageByMessageId(@PathVariable Integer messageId, @RequestBody Message newMessage) {
         // Logic to retrieve user profile
     }
+
+    // @RequestMapping(value = "/messages/{message_id}", method = RequestMethod.PATCH)
+  //  @PatchMapping("/messages/{message_id}")
+  //  public @ResponseBody Integer updatehMessageByMessageId(@PathVariable Integer messageId, @RequestParam String newMessage) {
+        // Logic to retrieve user profile
+  //  }
 
 
 
     // @RequestMapping(value = "/accounts/{account_id}/messages", method = RequestMethod.GET)
     @GetMapping("/accounts/{account_id}/messages")
-    public @ResponseBody List<Message> getAllMessagesByAccountId(@PathVariable Integer accountId) {
+    public ResponseEntity<List<Message>> getAllMessagesByAccountId(@PathVariable Integer accountId) {
+        private List<Message> messageList = new ArrayList<Message>();
         // Logic to authenticate user login
+        return ResponseEntity.status(200).body(messageList);
     }
 
 
+    }
 
     
-    @PostMapping("submit")
-    public ResponseEntity<String> submitInfo(@RequestParam String email,
-                                          @RequestParam String major,
-                                          @RequestParam int age,
-                                          @RequestParam String password){
-
-        studentList.add(new Student(email, major, age, password));
-        return ResponseEntity.status(201)
-                .body("Successfully Registered");
-
-    }
-
-    @PutMapping("update")
-    public @ResponseBody String update(@RequestBody Student updatedStudent){
-        if(studentList.removeIf(student -> student.getEmail().equals(updatedStudent.getEmail()))) {
-            studentList.add(updatedStudent);
-            return "Successfully updated";
-        }
-        return "Email was not registered, check email and try again. Or register new student.";
-    }
-
-    @GetMapping("info/{email}")
-    public @ResponseBody Student displayInfo(@PathVariable String email){
-
-        for (Student student:studentList){
-            if(student.getEmail().equals(email))
-                return student;
-        }
-        return null;
-
-    }
-
-    @PostMapping("/register")
-    public @ResponseBody String submit(@RequestParam String email,
-                                          @RequestParam String major,
-                                          @RequestParam int age,
-                                          @RequestParam String password){
-
-        studentList.add(new Student(email, major, age, password));
-        return "Successfully submitted";
-
-    }
+    
 
 }
