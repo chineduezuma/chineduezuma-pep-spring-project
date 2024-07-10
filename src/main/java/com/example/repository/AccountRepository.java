dcabeffa0198e7fb6ddb67995ed7d6a6cb92f035
Chinedu Ezuma <chineduezuma@yahoo.co.uk>
2024-07-10 05:27:59 +0000

    Prem 1

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,17 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    @Query("FROM Account WHERE username = :username")
+    Account getAccountByUsername(@Param("username") String username);
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,18 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    @Query("FROM Message WHERE postedBy = :accountId")
+    List<Message> getAllMessagesByAccountId(@Param("accountId") Integer accountId);
+    
 }
